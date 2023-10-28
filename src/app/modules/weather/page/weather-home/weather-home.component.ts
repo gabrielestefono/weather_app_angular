e23b45c573b5f1ff3bab05c24508ac48c95edb0a
gabrielestefono <gabrielestefono@hotmail.com>
2023-10-28 17:29:02 -0300

    Curso pausado

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,3 +1,4 @@
+import { WeatherService } from './../../services/weather.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class WeatherHomeComponent {
 
+  constructor(private WeatherService: WeatherService){}
 }
