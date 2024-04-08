6f1c38c6c186f0a45cf43e691948901621465a0a
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-04-08 18:16:36 -0300

    Consumindo serviço da aplicação e adicionando imagens

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherData } from 'src/app/modules/models/interfaces/weather.interface';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit{
+  constructor(private weatherService: WeatherService){}
+  public weatherData!: WeatherData;
 
+  ngOnInit(): void {
+    this.getWeatherData('Cascavel');
+  }
+
+  getWeatherData(cidade: string): void
+  {
+    this.weatherService.getWeatherData(cidade).subscribe({
+      next: (response) => {
+        response && (this.weatherData = response);
+        console.log(this.weatherData)
+      },
+      error: (error) => console.log(error)
+    })
+  }
 }
