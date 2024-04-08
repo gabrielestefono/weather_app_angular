ff5362252446ac8574d8b3b3cee9ca3624d7abd2
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-04-08 13:19:25 -0300

    Reiniciando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { WeatherHomeComponent } from './modules/weather/page/weather-home/weather-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WeatherHomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    FontAwesomeModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
